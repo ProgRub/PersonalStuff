de4f5922aba53ac2e067527682cd71ce9c66671b
ProgRub <ruben2000jgr@gmail.com>
2020-08-17 09:59:29 +0100

    Separated from Handler and Downloader because it's common

---
+++ Screen.py
@@ -0,0 +1,103 @@
+import tkinter as TK
+from ListsAndFiles import ListsAndFiles
+
+class Screen:
+    container = ListsAndFiles()
+    window = TK.Tk()
+    DEFAULT_FONT1 = ("Times New Roman", 16)
+    DEFAULT_FONT2 = ("Times New Roman", 14)
+    DEFAULT_FONT3 = ("Times New Roman", 12)
+    DEFAULT_BGCOLOR = "#061130"
+    SCROLLSPEED = 30  #less=faster
+
+    def __init__(self, masterFramePreviousScreen):
+        masterFramePreviousScreen.destroy()
+
+        #Widget Creation
+        self.frm_master = TK.Frame(Screen.window, bg=Screen.DEFAULT_BGCOLOR)
+        self.btn_backScreen = TK.Button(self.frm_master,
+                                        text="Go Back",
+                                        command=self.backScreen,
+                                        font=Screen.DEFAULT_FONT3)
+
+        #Widget Placement
+        self.frm_master.grid(row=0, column=0)
+
+        Screen.window.bind("<Return>", self.nextScreen)
+        Screen.window.bind('<KP_Enter>', self.nextScreen)
+        Screen.window.bind("<Escape>", self.backScreen)
+
+    @staticmethod
+    def removeWordsFromWord(setOfWords, word):
+        dentroParenteses = True
+        firstCicle = False
+        if word.find("(") != -1 or word.find("[") != -1:
+            for wordToRemove in setOfWords:
+                while wordToRemove in word:
+                    if dentroParenteses:
+                        pos1parentes = word.find("(")
+                        pos2parentes = word.find(")")
+                    if pos1parentes == -1 or not dentroParenteses:
+                        pos1parentes = word.find("[")
+                        pos2parentes = word.find("]")
+                        if pos1parentes == -1:
+                            dentroParenteses = True
+                            firstCicle = True
+                    if not firstCicle:
+                        if wordToRemove in word[pos1parentes - 1:pos2parentes +
+                                                1]:
+                            word = Screen.removeWordsFromWord(
+                                setOfWords,
+                                word.replace(
+                                    word[pos1parentes - 1:pos2parentes + 1],
+                                    ""))
+                        else:
+                            dentroParenteses = False
+                    else:
+                        aux = word[word.find(")") + 1:]
+                        for wordToRemove in setOfWords:
+                            while wordToRemove in word:
+                                if dentroParenteses:
+                                    pos1parentes = aux.find("(")
+                                    pos2parentes = aux.find(")")
+                                if pos1parentes == -1 or not dentroParenteses:
+                                    pos1parentes = aux.find("[")
+                                    pos2parentes = aux.find("]")
+                                    if pos1parentes == -1:
+                                        dentroParenteses = True
+                                if wordToRemove in aux[pos1parentes -
+                                                       1:pos2parentes + 1]:
+                                    aux2 = aux.replace(
+                                        aux[pos1parentes - 1:pos2parentes + 1],
+                                        "")
+                                    word = Screen.removeWordsFromWord(
+                                        setOfWords, word.replace(aux, aux2))
+                                else:
+                                    dentroParenteses = False
+        return word
+
+    @staticmethod
+    def correctRapGenre(genre):
+        return "Rap" if "Rap" in genre else genre
+
+    @staticmethod
+    def inverseCorrectRapGenre(genre):
+        return "Rap/Hip Hop" if "Rap" in genre else genre
+
+    @staticmethod
+    def standardFormatTime(time):
+        return str(int(time // 60)) + ":" + ("0" if time % 60 < 10 else
+                                             "") + str(int(time % 60))
+
+    @staticmethod
+    def generateGenreTags(textBox):
+        for genre in Screen.container.genresColors:
+            textBox.tag_config(Screen.correctRapGenre(genre),
+                               foreground=Screen.container.genresColors[
+                                   Screen.correctRapGenre(genre)])
+
+    def backScreen(self, event=None):
+        pass
+
+    def nextScreen(self, event=None):
+        pass
